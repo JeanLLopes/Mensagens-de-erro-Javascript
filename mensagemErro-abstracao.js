ff606b71a1ee75ca3e0288a0730a03c21b887fa7
JeanLLopes <jean.llopes@outlook.com>
2017-04-25 22:31:54 -0300

    Add file.js

---
+++ mensagemErro-abstracao.js
@@ -0,0 +1,10 @@
+function warm(msg) {
+    console.log(["AVISO: ", msg].join(' '));
+}
+
+function note(msg) {
+    console.log(["NOTA: ", msg].join(' '))
+}
+
+console.log(warm("Mensagem de Erro"))
+console.log(note("Mensagem de Erro"))
